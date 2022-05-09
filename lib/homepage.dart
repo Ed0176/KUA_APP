692b978e10295acc601f6fe36038b005d545d636
FlutLab.io <>
2022-05-09 07:48:42 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
                 "Continue Watching",
                 style: TextStyle(
                   fontFamily: 'SourceSansPro',
-                  fontSize: 25.0,
+                  fontSize: 10.0,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 1.0,
                   color: Colors.orange,
