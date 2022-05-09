85baedef70ee0c1a6179896f3b83ddae13cbae65
FlutLab.io <>
2022-05-09 07:57:20 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -57,25 +57,6 @@ class _HomePageState extends State<HomePage> {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 const ListTile(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    TextButton(
-                      child: const Text('BUY TICKETS'),
-                      onPressed: () {
-                        /* ... */
-                      },
-                    ),
-                    const SizedBox(width: 8),
-                    TextButton(
-                      child: const Text('LISTEN'),
-                      onPressed: () {
-                        /* ... */
-                      },
-                    ),
-                    const SizedBox(width: 8),
-                  ],
-                ),
               ],
             ),
           ),
