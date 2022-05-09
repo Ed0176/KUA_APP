feed08015c7d11dc9af24aaf44fb30af3119d610
FlutLab.io <>
2022-05-09 06:04:29 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _LoginState extends State<Login> {
               letterSpacing: 2.0,
               color: Colors.orange,
             ),
+            textAlign: TextAlign.center,
           ),
           SizedBox(
             width: 150.0,
