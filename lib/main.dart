33823285454840e6f906e96fb568d6da60f37d77
FlutLab.io <>
2022-05-09 07:12:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -93,7 +93,7 @@ class _LoginState extends State<Login> {
                 fillColor: Colors.grey[900],
                 filled: true,
                 prefixIcon: Icon(
-                  Icons.lock,
+                  Icons.key,
                   color: Colors.orange,
                 ),
                 enabledBorder: OutlineInputBorder(
