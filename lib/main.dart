d455679f8cac475e473cb31f23d270c47ea31471
FlutLab.io <>
2022-05-09 07:44:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:math';
+import 'homepage.dart';
 
 void main() {
-  runApp(Login());
+  runApp(HomePage());
 }
 
 class Login extends StatefulWidget {
