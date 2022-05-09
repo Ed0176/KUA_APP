b94696e4a33b329518d3c3ceea5e2043d2cca660
FlutLab.io <>
2022-05-09 09:26:55 +0000

    Autosave

--- lib/try.dart
+++ lib/try.dart
@@ -4,7 +4,7 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     const title = 'Horizontal List';
