6e69ca6ff05df6bb985c96e7cb6d69b2b92b0cfd
FlutLab.io <>
2022-05-09 06:55:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _LoginState extends State<Login> {
             child: TextField(
               obscureText: false,
               decoration: InputDecoration(
-                hintStyle: TextStyle(color: Colors.red),
+                hintStyle: TextStyle(color: Colors.white),
                 prefixIcon: Icon(Icons.account_circle, color: Colors.amber),
                 enabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.white, width: 2.0),
