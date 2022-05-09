e2f7f79b9dcb96730dd19d4508e9a609442d16a9
FlutLab.io <>
2022-05-09 07:45:54 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,6 +12,134 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
         home: Scaffold(
       backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Welcome Back",
+                style: TextStyle(
+                  fontFamily: 'WaterBrush',
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.orange,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: EdgeInsets.all(0.0),
+            child: TextField(
+              obscureText: false,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                fillColor: Colors.grey[900],
+                filled: true,
+                hintStyle: TextStyle(color: Colors.white),
+                prefixIcon: Icon(Icons.account_circle, color: Colors.orange),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.amber, width: 2.0)),
+                border: OutlineInputBorder(),
+                labelText: 'Username or phonenumber',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Padding(
+            padding: EdgeInsets.all(0.0),
+            child: TextField(
+              style: TextStyle(color: Colors.white),
+              obscureText: true,
+              decoration: InputDecoration(
+                fillColor: Colors.grey[900],
+                filled: true,
+                prefixIcon: Icon(
+                  Icons.password,
+                  color: Colors.orange,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber, width: 2.0),
+                ),
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ButtonTheme(
+            minWidth: 350.0,
+            height: 50.0,
+            child: RaisedButton(
+              color: Colors.orange,
+              textColor: Colors.white,
+              shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(3.0),
+              ),
+              onPressed: () {},
+              child: Text('Login'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 90.0, vertical: 20.0),
+            child: Row(children: [
+              Text("Not Registered?", style: TextStyle(color: Colors.white)),
+              TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(8.0),
+                  primary: Colors.white,
+                  textStyle: const TextStyle(fontSize: 12, color: Colors.amber),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'REGISTER',
+                  style: TextStyle(fontSize: 12, color: Colors.orange),
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
     ));
   }
 }
