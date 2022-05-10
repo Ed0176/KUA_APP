536f1df345dc401bff05c12d6c53031962342355
FlutLab.io <>
2022-05-10 11:20:53 +0000

    Autosave

---
+++ lib/list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class List extends StatefulWidget {
+  const List({Key? key}) : super(key: key);
+  _ListState createState() => _ListState();
+}
+
+class _ListState extends State<List> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(children: [
+      buildCard(),
+      SizedBox(width: 10.0),
+      buildCard(),
+      SizedBox(width: 10.0),
+      buildCard(),
+      SizedBox(width: 10.0),
+      buildCard(),
+      SizedBox(width: 10.0),
+    ]));
+  }
+
+  Widget buildCard() => Container(width: 200.0, height: 200.0, color: Colors.red);
+}
