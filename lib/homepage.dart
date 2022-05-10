6c039907c938a91b4164652b9bcd714a1652091f
FlutLab.io <>
2022-05-10 09:15:55 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -76,9 +76,7 @@ class _HomePageState extends State<HomePage> {
                 Card(
                   //width: 160.0,
                   color: Colors.grey,
-                  child: Expanded(
-                    child: Image.asset('images/flower.jpg', height: 100.0, width: 160),
-                  ),
+                  child: Image.asset('images/flower.jpg', height: 100.0, width: 160),
                 ),
                 SizedBox(
                   width: 10.0,
@@ -86,12 +84,15 @@ class _HomePageState extends State<HomePage> {
                 Card(
                   //width: 160.0,
                   color: Colors.grey,
+                  child: Image.asset('images/flower.jpg', height: 100.0, width: 160),
                 ),
                 Card(
                   color: Colors.grey,
+                  child: Image.asset('images/flower.jpg', height: 100.0, width: 160),
                 ),
                 Card(
                   color: Colors.grey,
+                  child: Image.asset('images/flower.jpg', height: 100.0, width: 160),
                 ),
               ],
             ),
