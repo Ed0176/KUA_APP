7ff2ff142bddfe906e5bf86eddc58bed8264cbf9
FlutLab.io <>
2022-05-10 09:28:33 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -76,7 +76,11 @@ class _HomePageState extends State<HomePage> {
                 Card(
                   //width: 160.0,
                   color: Colors.grey,
-                  child: Image.asset('images/flower.jpg', height: 100.0, width: 160),
+                  child: ListTile(
+                    title: Text('Diamond'),
+                    subtitle: Text('Music '),
+                    backgroundImage: Image.asset('images/flower.jpg', height: 100.0, width: 160),
+                  ),
                 ),
                 SizedBox(
                   width: 10.0,
